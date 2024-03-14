9f6c14769d990c3f709a5145e13a038d99d0303d
Abhishek Ranjan <119099659+abhi-2029@users.noreply.github.com>
2024-03-14 20:44:13 +0530

    Add files via upload

---
+++ eligibility.cpp
@@ -0,0 +1,18 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int age;
+//     cout<<"Enter the age: ";
+//     cin>>age;
+//     if(age<=15){
+//         cout<<"You are not eligible for riding";
+//     }else if(age>=15&& age<20){
+//         cout<<"You are not eligible for highway riding";
+//     }else if(age>=21 && age<30){
+//         cout<<"You are eligible for highway bike riding";
+//     }else if(age>=31 && age<50){
+//         cout<<"You are eligible for car riding ";
+//     }
+//     cout<<endl;
+//     return 0;
+// }
