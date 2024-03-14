7aecd1de91376ca6f0118c91ac7c7b29e045a5fd
Abhishek Ranjan <119099659+abhi-2029@users.noreply.github.com>
2024-03-14 20:35:23 +0530

    Add files via upload

---
+++ grade_calculation.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int marks;
+//     cout<<"Enter the marks:";
+//     cin>>marks;
+//     if(marks>=90){
+//         cout<<"Topper";
+//     }else if(marks>=80 && marks<90){
+//         cout<<"Second Topper";
+//     }else if(marks>=70 && marks<80){
+//         cout<<"Third Topper";
+//     }else if(marks>=60 && marks<70){
+//         cout<<"Just Pass";
+//     }else{
+//         cout<<"Fail";
+//     }
+//     cout<<endl;
+//     return 0;
+// }
