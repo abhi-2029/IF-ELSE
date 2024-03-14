22d3bb539ffe741c052afcdeeaa2ced7159f8054
Abhishek Ranjan <119099659+abhi-2029@users.noreply.github.com>
2024-03-14 20:40:05 +0530

    Add files via upload

---
+++ print_square.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int length, breadth;
+//     cout<<"Enter the value of length: ";
+//     cin>>length;
+//     cout<<"Enter the value of breadth: ";
+//     cin>>breadth;
+//     if(length==breadth){
+//         cout<<"Square";
+//     }else{
+//         cout<<"Not Square";
+//     }
+//     cout<<endl;
+//     return 0;
+// }
